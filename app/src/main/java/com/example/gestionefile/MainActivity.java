d0acfc0f2d579da59300456d7e8a51a2d4bd423b
MBartoccioni <matteo.barto57@gmail.com>
2022-02-18 00:57:40 +0100

    Esercitazione n.4

---
+++ app/src/main/java/com/example/gestionefile/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.gestionefile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.gestionefile.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLeggi;
+    Button btnScrivi;
+    EditText txtContenuto;
+    Gestore gf;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLeggi= (Button)findViewById(R.id.btnLeggi);
+        btnScrivi= (Button)findViewById(R.id.btnScrivi);
+        txtContenuto= findViewById(R.id.txtContenuto);
+        gf= new Gestore("nomeFile.txt");
+        btnLeggi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sb=  gf.readFile("nomeFile.txt", getApplicationContext());
+                Toast.makeText(getApplicationContext(), sb, Toast.LENGTH_LONG ).show();
+            }
+        });
+        btnScrivi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sc= gf.scriviFile("nomeFile.txt", getApplicationContext());
+                Toast.makeText(getApplicationContext(), sc, Toast.LENGTH_LONG ).show();
+            }
+        });
+
+    }
+}
